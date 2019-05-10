3d6803b4ee93c67563462e81a37b85ec9373fe9a
Taras Lutsiuk <34063742+Farleo@users.noreply.github.com>
2019-05-10 19:50:43 +0300

    Create InsertSql.sql
    create insert sql query

---
+++ InsertSql.sql
@@ -0,0 +1,2 @@
+INSERT INTO JavaWebApp.user (id, email, password, name, date_created, date_last_entered)
+VALUES (1, 'test@test.com', '1111', 'Tester', '2019-04-22 12:08:10', '2019-04-22 12:30:11');
