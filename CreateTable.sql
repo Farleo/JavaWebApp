656defce8b5176ebf0990b72991d65598e070ea2
Taras Lutsiuk <34063742+Farleo@users.noreply.github.com>
2019-05-10 19:48:38 +0300

    Update and rename sql.sql to CreateTable.sql
    split sql query

--- CreateTable.sql
+++ CreateTable.sql
@@ -10,7 +10,3 @@ create table user
   constraint user_email_uindex
     unique (email)
 );
-
-
-INSERT INTO JavaWebApp.user (id, email, password, name, date_created, date_last_entered)
-VALUES (1, 'test@test.com', '1111', 'Tester', '2019-04-22 12:08:10', '2019-04-22 12:30:11');
